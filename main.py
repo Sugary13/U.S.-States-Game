787f5bdd564c7331584f43b5744c76e57551d64a
Sugary13 <146895409+Sugary13@users.noreply.github.com>
2025-06-07 23:23:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 states_list = data.state.to_list()
-x_list = data.x.to_list()
-y_list = data.y.to_list()
 text.hideturtle()
 text.penup()
 
@@ -24,25 +22,24 @@ while correct_answers < 50:
     answer_state = screen.textinput(title= f"{correct_answers}/50 States Correct",
                                              prompt="What is another the state's name?")
     if not answer_state:
+        for state in guess_list:
+            states_list.remove(state)
+        df = pandas.DataFrame(states_list)
+        df.to_csv("states_to_learn.csv")
         break
     else:
         answer_state = answer_state.title()
 
     if answer_state in states_list and answer_state not in guess_list:
         guess_list.append(answer_state)
-        answer_index = states_list.index(answer_state)
-        x = x_list[answer_index]
-        y = y_list[answer_index]
-        text.goto(x, y)
+        state_data = data[data.state == answer_state]
+        text.goto(state_data.x.item(), state_data.y.item())
         text.write(answer_state)
         print(f"{answer_state} is correct!.")
         correct_answers += 1
 
     else:
         print(f"{answer_state} is not correct or you already guessed it.")
-
-
-
 
 
 screen.exitonclick()
